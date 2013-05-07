051fbe945c9a283377c07c6c35e4627079f9c02e
Javier Wilson <javier.wilson@gmail.com>
2013-05-07 15:32:34 -0400

    cambiando paginas por noticias en portada

--- website/views.py
+++ website/views.py
@@ -1,18 +1,16 @@
-from models import Blog, Foto, Pagina, TipoAutor, Enlace
+from models import Blog, Foto, Noticia, Enlace
 from django.shortcuts import render_to_response
 from django.views.generic import DetailView
 
 def home(request):
-    paginas = Pagina.objects.all()[:3]
+    noticias = Noticia.objects.all()[:3]
     blogs = Blog.objects.order_by('-fecha')[:3]
     enlaces = Enlace.objects.all()[:5]
-    tipo_autor = TipoAutor.objects.all()
 
     return render_to_response('home.html',{
-        'paginas': paginas,
+        'noticias': noticias,
         'blogs': blogs,
         'enlaces': enlaces,
-        'tipo_autor': tipo_autor,
     })
 
 class BlogDetailView(DetailView):
